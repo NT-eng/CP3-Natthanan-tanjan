84b1045f50db1de02c7b0a9fc21f324223a16fc2
Eng2545 <chelsea2002eng@gmail.com>
2021-05-08 15:38:43 +0700

    Create Exercise4_natthanan_.py

---
+++ Exercise4_natthanan_.py
@@ -0,0 +1,9 @@
+FE=75
+GB=68.4
+ITCS=70
+CP=80
+print("---Your score---")
+print( 'Foundation English               :',FE,)
+print('General Business                 :',GB)
+print('Introduction to Computer Systems :',ITCS)
+print("Computer Programming             :", CP)
