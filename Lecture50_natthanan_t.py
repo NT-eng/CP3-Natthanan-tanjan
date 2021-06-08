54b463fdf9854db1294cddbe9630817194835146
Eng2545 <chelsea2002eng@gmail.com>
2021-06-08 14:12:40 +0700

    Lecture50

---
+++ Lecture50_natthanan_t.py
@@ -0,0 +1,12 @@
+def plusnumber(x,y):
+    print(x,"+",y,"=",x+y)
+def minus(x,y):
+    print(x,"-",y,"=",x-y)
+def multiply(x, y):
+    print(x,"*",y,"=",x*y)
+def divide(x,y):
+    print(x,"/",y,"=",x/y)
+plusnumber(50,25)
+minus(5,2)
+multiply(25,25)
+divide(60,12)
